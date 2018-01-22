fe63884c99b0e8f5bc3981a74708d29245bc1352
Benjamin Bowes <bbowes999@gmail.com>
2018-01-22 14:24:38 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,15 @@
+var playlist = { 
+  Slowdive:'Alison', 
+  MY Bloody Valentine:'Sometimes'
+  
+};
+
+function updateplaylist (playlist, artistName, songTitle){
+  playlist[Phil Ochs] = Here's to the State of Mississippi
+  return playlist
+  
+}
+function removeFromPlaylist (playlist,artistName ){
+  delete playlist{Slowdive}
+  return playlist
+}
